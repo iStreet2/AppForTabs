a05b08047a44750013266070e2af177a9dd2bdde
Gabriel Vicentin Negro <gabrielvicentinnegro@hotmail.com>
2023-09-25 13:46:19 -0300

    Initial Commit

---
+++ AppForTabs/AppForTabsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppForTabsApp.swift
+//  AppForTabs
+//
+//  Created by Gabriel Vicentin Negro on 25/09/23.
+//
+
+import SwiftUI
+
+@main
+struct AppForTabsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
