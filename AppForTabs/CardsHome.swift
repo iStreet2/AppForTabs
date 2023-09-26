37de74a188f07de7687e8aed827c555490eace59
bia-andreucci02 <124587486+bia-andreucci02@users.noreply.github.com>
2023-09-26 15:09:14 -0300

    tela home

---
+++ AppForTabs/CardsHome.swift
@@ -0,0 +1,18 @@
+//
+//  CardsHome.swift
+//  AppForTabs
+//
+//  Created by Beatriz Andreucci on 25/09/23.
+//
+
+import SwiftUI
+
+struct CardsHome: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardsHome()
+}
