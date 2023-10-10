500b1bcee97d8872a2900982076e4dde3cfc4cbf
bia-andreucci02 <124587486+bia-andreucci02@users.noreply.github.com>
2023-10-10 15:02:28 -0300

    arrumar dynamic type

---
+++ AppForTabs/Views/TesteView.swift
@@ -0,0 +1,18 @@
+//
+//  TesteView.swift
+//  AppForTabs
+//
+//  Created by Beatriz Andreucci on 09/10/23.
+//
+
+import SwiftUI
+
+struct TesteView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TesteView()
+}
